4e9c91844058bd8e63e21cecb3f72682d4fb8f82
KV34 <48414678+KV34@users.noreply.github.com>
2019-03-11 19:51:10 +0000

    Add files via upload

---
+++ TheGUI.cpp
@@ -0,0 +1,199 @@
+/*
+ * Sample code for android application https://play.google.com/store/apps/details?id=com.anypackagehost.unproject_rubic
+*/
+
+#include "TheGUI.h"
+#include "SubWindow.h"
+#include "SettingsControls.h"
+#include "GameControl.h"
+#include "Records.h"
+#include "DebugGUI.h"
+
+class MenuButton:public CustomButton
+{
+public:
+    MenuButton(Layout &l):CustomButton(l,FTCenter,FTTop)
+    {
+        Size sz=getTextSize("M").expandedOn(GUIStyle::current().text_padding);
+        sz._w=sz._h=std::max(sz._w,sz._h);
+        setFixedSize(sz);
+    }
+    void onSetupControlStates(WindowRenderer::StateMachine &sm)override
+    {
+        ASSERT(rect().width()==rect().height());
+        float r=rect().width()/2;
+        const GLColor norm_color=GUIStyle::current().frame_color;
+        const GLColor hot_color=GUIStyle::current().hot_color;
+        int shape_n=0;
+        sm.updateHollowCircle(shape_n,rect().center(),r);
+        sm.setupState(Normal,shape_n,norm_color);
+        sm.setupState(Pressed,shape_n++,hot_color);
+        r-=GUIStyle::current().text_padding;
+        float len=r*sqrt(2);
+        float offset=(rect().width()-len)/2;
+        float h_step=len/5;
+        for(int i=0;i<3;i++)
+        {
+            pt2d bl=rect().corner(Rect::BottomLeft)+vc2d{offset,offset+i*2*h_step};
+            pt2d tr{bl.x+len,bl.y+h_step};
+            sm.updateRectShape(shape_n,ShapeType::SolidRect,Rect::from2Edges(bl,tr));
+            sm.setupState(Normal,shape_n,norm_color);
+            sm.setupState(Pressed,shape_n++,hot_color);
+        }
+    }
+};
+
+class TimerLabel: public DynamicTextWindow
+{
+public:
+    TimerLabel(Layout &l):DynamicTextWindow("00:00",l,FTCenter,FTBottom)
+    {
+        adjustSizeToValues({"100:00","12:22"});
+    }
+protected:
+    void onTimeUpdate(float delta)override
+    {
+        if(!GameControl::isInGame())
+            setText("Solved");
+        else
+            setText(secToTimeStr(settings().tmp_timer));
+    }
+    void draw()const override
+    {
+        if(!GameControl::isShufflingTheCube())
+        {
+            if(!GameControl::isInGame()||settings().show_timer)
+                DynamicTextWindow::draw();
+        }
+    }
+};
+
+
+///all GUI controls with handlers are here:
+TheGUI::TheGUI():_block_other_views(false)
+{
+    VerticalLayout* overlay=new VerticalLayout(desktop(),FTCenter,FTStretch);
+    overlay->_draw_border=false;
+    overlay->_outer_border=overlay->_inner_border=0;
+    MenuButton* menu_btn=new MenuButton(*overlay);
+    new TimerLabel(*overlay);
+
+    std::function<void(SubWindow*)> show=[overlay,this](SubWindow* w)
+    {
+        GameControl::gui_is_ready_for_ad=w!=overlay;
+        if(Dialog* dlg=dynamic_cast<Dialog*>(w))
+        {
+            dlg->open();
+            GameControl::gui_is_ready_for_ad=false;
+        }
+        else
+        {
+            if(dynamic_cast<RecordsTable*>(w))
+                GameControl::gui_is_ready_for_ad=false;
+            desktop().setWindow(*w);
+        }
+        this->_block_other_views=w!=overlay;
+    };
+
+    VoidFunc startTheGame=[=]()
+    {
+        show(overlay);
+        GameControl::startTheGame();
+    };
+
+    //MENU:
+    Menu* main_menu=new Menu(desktop(),FTCenter,FTCenter);
+    main_menu->addItem("NEW GAME");
+    main_menu->addItem("NEW GAME...");
+    main_menu->addItem("OPTIONS");
+    Menu::Item* menu_back=main_menu->addItem("BACK");
+    //main_menu->addItem("TEST");
+    main_menu->addItem("ABOUT");
+    main_menu->addItem("QUIT");
+
+    VoidFunc showMainMenu=[show,main_menu,menu_back](){
+        show(main_menu);
+        menu_back->setEnabled(GameControl::isInGame());
+    };
+
+    menu_btn->setClickHandler([=]()
+    {   showMainMenu();});
+
+
+    //New custom game:
+    HeaderLayout* new_game_params=new HeaderLayout("Parameters",desktop(),FTCenter,FTCenter);
+    new IntSpinBox(settings().shuffle_steps,"Shuffle turns:",{1,50},1,*new_game_params);
+    new IntSpinBox(settings().cube_size,"Cube size:",{2,3},1,*new_game_params);
+    HorButtonsGroup* btn_group=new HorButtonsGroup(*new_game_params);
+    btn_group->addBtn("Back")->setClickHandler([=](){showMainMenu();});
+    btn_group->addBtn("Start")->setClickHandler([=](){startTheGame();});
+
+    //Options:
+    HeaderLayout* opts_header=new HeaderLayout("Options",desktop(),FTCenter,FTCenter);
+    VerticalLayout* options=new VerticalLayout(*opts_header,FTStretch,FTStretch);
+    options->_outer_border=options->_inner_border=0;
+    options->_draw_border=false;
+    new SpinBox(settings().animation_speed, "Animation speed:",{1,20},1,*options);
+    new BoolEnumControl(settings().show_shuffle_animation,"Suffle animation:",{"OFF","ON"},*options);
+    new BoolEnumControl(settings().show_timer,"Timer:",{"HIDDEN","VISIBLE"},*options);
+    //+new DualEnumBox("Cube place:",{"LEFT","RIGHT"},*options);
+    //+show clock
+    HorButtonsGroup* opts_btn_group=new HorButtonsGroup(*options);
+    opts_btn_group->addBtn("Accept")->setClickHandler([=](){showMainMenu();});
+
+    //Menu handlers:
+    main_menu->setItemHandler([=](ItemId item)
+    {
+        if(item=="OPTIONS")
+            show(opts_header);
+        else if(item=="BACK")
+            show(overlay);
+        else if(item=="NEW GAME")
+            startTheGame();
+        else if(item=="NEW GAME...")
+            show(new_game_params);
+        else if(item=="QUIT")
+            GameControl::quit();
+        else if(item=="ABOUT")
+            Dialog::execute("Credits(3rd party)","This software uses part of the\n"
+                                                 "Freetype library(www.freetype.org),\n"
+                                                 "under the terms of the FT License."
+                                                 ,{"OK"});
+        else if(item=="TEST")
+            Dialog::execute("!",DebugVars::getVarsString(),{"OK"});
+        else
+            UNEXPECTED;
+    });
+
+    //After win:
+    RecordsTable* rec_tab=new RecordsTable(desktop(),FTCenter,FTCenter);
+    rec_tab->_close_btn->setClickHandler([=](){show(overlay);});
+    Dialog* win_dlg=new Dialog("*Congratulations*","You win!",{"OK","RECORDS"});
+    win_dlg->setMinimalSize(TextRect("__Congratulations__",true).size());
+    win_dlg->setBtnHandler([=](ButtonId btn)
+    {
+        if(btn=="RECORDS")
+        {
+            win_dlg->close();
+            rec_tab->reArrange();
+            show(rec_tab);
+        }
+        else
+            this->_block_other_views=false;
+    });
+    GameControl::setVictoryHandler([=]()
+    {
+        this->_block_other_views=true;
+        info<<"you've lost"<<secToTimeStr(settings().tmp_timer);
+        show(win_dlg);
+    });
+
+    showMainMenu();
+}
+
+ViewsConcurency TheGUI::viewConcurency() const
+{
+    if(_block_other_views)
+        return ViewsConcurency::ExclusiveDrawing|ViewsConcurency::ExclusiveMouseHandling;
+    return ViewsConcurency::None;
+}
